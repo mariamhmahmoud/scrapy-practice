af7d34ed399fa911e76c1e9cb50a4a905f45dcaa
mariamhm <mariamhm@umich.edu>
2020-11-29 16:18:01 -0500

    Scrape subpages for title

--- spiders/books_spider.py
+++ spiders/books_spider.py
@@ -9,20 +9,32 @@ class BooksSpider(scrapy.Spider):
 
 	def parse(self, response):
 
-		items = ScrapePracticeItem()
-
 		for book in response.css('li .product_pod'):
+			items = ScrapePracticeItem()
+
 			title = book.css('h3 a::text').get()
 			price = book.css('.product_price p::text').get()
 			rating = str(book.xpath('.//p[contains(@class, "star-rating")]/@class').re(r'star-rating(.*)$'))
 
-			items['title'] = title
 			items['price'] = price 
 			items['rating'] = rating
 
-			yield items
+			subpage = book.css('h3 a::attr(href)').get()
+			subpage = response.urljoin(subpage)
+
+			request = scrapy.Request(subpage, callback=self.scrapeTitle)
+			request.meta['items'] = items
+
+			yield request
 
 		next_page = response.css('.next a::attr(href)').get()
 		if next_page is not None:
 			next_page = response.urljoin(next_page)
 			yield scrapy.Request(next_page, callback = self.parse)
+
+
+	
+	def scrapeTitle(self, response):
+		items = response.meta['items']
+		items['title'] = response.css('h1::text').get()
+		yield items
